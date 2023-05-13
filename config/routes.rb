51ae625163c20cdfb0696e3f0b2fea436eee2c31
Ozeyuta <ozeyuta.jkk@gmail.com>
2023-05-13 18:03:04 +0900

    ビューの調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'protos#index'
-  resources :protos, only: [:index, :new, :create, :show]
+  resources :protos, only: [:index, :new, :create, :show, ]
 end
